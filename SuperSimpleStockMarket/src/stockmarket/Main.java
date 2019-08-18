158b2a31b880dd5b8abd414960960577eb571c29
Prateek Garg <prateek.garg23@gmail.com>
2019-08-18 23:20:17 +0100

    Formatting of main class

--- SuperSimpleStockMarket/src/stockmarket/Main.java
+++ SuperSimpleStockMarket/src/stockmarket/Main.java
@@ -1,7 +1,5 @@
 package stockmarket;
 
-
-
 import java.io.IOException;
 import java.util.Scanner;
 
@@ -9,19 +7,19 @@ import stockmarket.model.GBCEDataModelSingleton;
 import stockmarket.util.SelectionPage;
 
 public class Main {
-	
+
 	static {
 		GBCEDataModelSingleton.getStockDataModel();
 	}
-	
-	public static void  main (String [] args) throws IOException, InterruptedException {
-		
+
+	public static void main(String[] args) throws IOException, InterruptedException {
+
 		System.out.println("Welcome TO Stock Market");
 		System.out.println("\nPress Any TO Proceed ... \n");
 		Scanner scanner = new Scanner(System.in);
-        scanner.nextLine();
-        SelectionPage.showAndSelectOptions(scanner);
-          
+		scanner.nextLine();
+		SelectionPage.showAndSelectOptions(scanner);
+
 	}
-		
+
 }
